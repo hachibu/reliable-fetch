b3ccf2709b67b51527201be6514fd8c2dab9a994
hachibu <raymondsohn@gmail.com>
2022-10-08 20:33:49 -0700

    Remove ReliableFallbackFunction type

--- src/types.ts
+++ src/types.ts
@@ -5,14 +5,12 @@ export type ReliableFetchFunction = (
     init?: ReliableRequestInit
 ) => Promise<Response>
 
-export type ReliableFallbackFunction = () => Promise<Response>
-
 export type ReliableRequestInfo = RequestInfo | URL
 
 export type ReliableRequestInit = RequestInit &
     Options & {
         backoff?: RetryBackoffStrategy
-        fallback?: ReliableFallbackFunction
+        fallback?: () => Promise<Response>
         fetch?: ReliableFetchFunction
         retries?: number
         delay?: number
