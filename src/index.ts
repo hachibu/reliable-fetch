fc2304be96337b93e2439240406f6091f46d9cd2
hachibu <raymondsohn@gmail.com>
2022-10-08 20:37:20 -0700

    Refactor reliableFetch function

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,7 @@ import fetchRetry from './fetchRetry'
 const reliableFetch = (
     url: ReliableRequestInfo,
     init?: ReliableRequestInit
-): ReliableFetch => {
-    return new ReliableFetch(url, init)
-}
+): ReliableFetch => new ReliableFetch(url, init)
 
 export default reliableFetch
 export * from './types'
