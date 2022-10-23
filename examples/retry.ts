e5be8c84289c29b67c5033aa6addcdce0b22efc2
hachibu <raymondsohn@gmail.com>
2022-10-22 20:19:09 -0700

    Update retry example

--- examples/retry.ts
+++ examples/retry.ts
@@ -1,7 +1,12 @@
 import reliableFetch from '../src/index'
 
 async function main() {
-    const res = await reliableFetch('https://google.com').retry()
+    const res = await reliableFetch('https://google.com').retry({
+        delay: 100,
+        retries: 3,
+        strategy: 'exponential',
+        jitter: true,
+    })
 
     console.log(res.status)
 }
