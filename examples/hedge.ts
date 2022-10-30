1d457ea5cd09000cbcb0e4b463681fe559343b42
hachibu <raymondsohn@gmail.com>
2022-10-29 19:35:11 -0700

    Update hedge example

--- examples/hedge.ts
+++ examples/hedge.ts
@@ -1,11 +1,11 @@
 import reliableFetch from '../src/index'
 
 async function main() {
-    const res = await reliableFetch('https://google.com')
+    const response = await reliableFetch('https://google.com')
         .on('hedge', () => console.log('hedge triggered'))
         .hedge({ timeout: 10 })
 
-    console.log(res.status)
+    console.log(response.status)
 }
 
 main()
