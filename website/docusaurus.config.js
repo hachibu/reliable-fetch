ed97f659b66aae3d33dcee0af3689ab364ed0a8b
Raymond Sohn <raymondsohn@gmail.com>
2022-12-30 16:58:48 -0800

    Configure Twitter card image

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -58,6 +58,7 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
+            image: 'img/logo.svg',
             navbar: {
                 title: 'Reliable Fetch',
                 logo: {
