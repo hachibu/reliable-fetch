a7c5cfaa6032b0c00c61d00f359a91a96fe6a40f
Raymond Sohn <raymondsohn@gmail.com>
2022-12-30 17:06:48 -0800

    Add PNG version of logo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            image: 'img/logo.svg',
+            image: 'img/logo.png',
             navbar: {
                 title: 'Reliable Fetch',
                 logo: {
